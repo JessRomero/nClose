136a126c0b818e9b96dd862fe09886672d9b2082
username <116606658+JessRomero@users.noreply.github.com>
2023-02-11 17:51:34 -0800

    use r-state, Client-side routing, basic navigation

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar';
+import NewOrderPage from '../NewOrderPage/NewOrderPage'
+import AuthPage from '../AuthPage/AuthPage'
+import ProductsPage from '../ProductsPage/ProductsPage'
+
+export default function App() {
+  const [user, setUser] = useState({})
+  return (
+    <main className="App">
+      <NavBar />
+      {user ?
+        <Routes>
+          <Route path="/orders" element={<NewOrderPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+        </Routes>
+        :
+        <AuthPage />
+      }
+    </main>
+  );
+}
+
+
+
